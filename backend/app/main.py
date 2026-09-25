20390eced69363699c0e555b94b8dcd065212adf
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-09-25 16:28:50 +0500

    fix cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,8 +8,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
         "https://blogify-three-rouge.vercel.app"
+        "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
